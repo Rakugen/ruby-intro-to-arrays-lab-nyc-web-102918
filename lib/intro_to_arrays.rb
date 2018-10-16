619d4a639bb787c3138f092b09728207d402d307
Simon Lee <simonlee0306@gmail.com>
2018-10-16 21:12:10 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  doggos = ["shiba", "inu"]
+end
+
+def first_element
+
+end
+
+def third_element
+
+end
+
+def last_element
+
+end
+
+def first_element_with_array_methods
+
+end
+
+def last_element_with_array_methods
+
+end
+
+def length_of_array
+
+end
