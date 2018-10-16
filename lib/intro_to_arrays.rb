fdf86f884289bdae5e85194fcfb72fad2ec87241
Simon Lee <simonlee0306@gmail.com>
2018-10-16 21:39:24 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,12 +14,12 @@ def third_element(array)
   array[2]
 end
 
-def last_element
+def last_element(array)
   array[-1]
 end
 
-def first_element_with_array_methods
-
+def first_element_with_array_methods(array)
+  array.shift
 end
 
 def last_element_with_array_methods
